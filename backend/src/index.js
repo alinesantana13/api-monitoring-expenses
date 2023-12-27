e42562dad0bac24712d566a3ccad1b0fff4932c3
Aline Santana <aline_santana13@hotmail.com>
2023-12-26 22:00:26 -0300

    docs: documentation with swagger registration and login route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(router);
 
